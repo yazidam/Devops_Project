d87aa12a296a27f3f5291cb1a4190a9512a8d276
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2021-10-13 21:46:56 +0100

    first commit

---
+++ src/main/java/com/example/demo/TimesheetSpringBootCoreDataJpaMvcRest2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetSpringBootCoreDataJpaMvcRest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetSpringBootCoreDataJpaMvcRest2Application.class, args);
+	}
+
+}
